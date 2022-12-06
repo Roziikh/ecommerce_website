238260bdb8cda4821df5dbdef29c968f46c6f9cf
Roziikh <rozii@bk.ru>
2022-12-06 17:24:59 +0400

    paypal & order history added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -21,6 +21,17 @@ orderRouter.post(
     res.status(201).send({ message: "New Order Created", order }); // froont-um uneinq order_id
   })
 );
+
+orderRouter.get(
+  "/mine",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+//returns orders of current user
+
 orderRouter.get(
   "/:id",
   isAuth,
@@ -33,4 +44,27 @@ orderRouter.get(
     }
   })
 );
+
+orderRouter.put(
+  "/:id/pay",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.send({ message: "Order is Paid", order: updatedOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
 export default orderRouter;
